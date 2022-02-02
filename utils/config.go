171001dd9d4c04aab94374e0566c84ca4a756ffd
Francisco Briones <franciscobrioneslavados@gmail.com>
2022-02-02 12:02:17 -0300

    :sparkles: adding viper to manage environment config and setup fix main test

---
+++ utils/config.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/spf13/viper"
+
+// COnfig stores all configurations of the application.
+// The values are ready by viper from a config file or a environment variables.
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig read configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
